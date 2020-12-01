3713603e8243d7c32923559851e524222c7b56f4
dapp-whisperer <dapp-whisperer@pm.me>
2020-12-01 15:41:39 -0600

    initial commit

---
+++ src/mapping.ts
@@ -0,0 +1,3 @@
+export { handleShareTransfer } from './mappings/yVault';
+export { handleHarvest } from './mappings/strategy';
+export { handleStaked, handleUnstaked } from './mappings/geyser';
---
+++ src/utils/helpers/geyser/geyser.ts
@@ -0,0 +1,91 @@
+import { Address, BigInt, log } from '@graphprotocol/graph-ts';
+
+import {
+  Controller,
+  Deposit,
+  Geyser,
+  Harvest,
+  StakedEvent,
+  Strategy,
+  Transaction,
+  Transfer,
+  UnstakedEvent,
+  Vault,
+  Withdrawal,
+} from '../../../../generated/schema';
+import { Strategy as StrategyABI } from '../../../../generated/templates';
+import { Controller as ControllerContract } from '../../../../generated/nativeBadgerSett/Controller';
+import { Strategy as StrategyContract } from '../../../../generated/nativeBadgerSett/Strategy';
+import { V1Contract } from '../../../../generated/nativeBadgerSett/V1Contract';
+import { BadgerGeyser, Staked, Unstaked } from '../../../../generated/nativeBadgerGeyser/BadgerGeyser';
+import { BIGDECIMAL_ZERO, BIGINT_ZERO } from '../../constants';
+import { toDecimal } from '../../decimals';
+import { getOrCreateToken } from '../yVault/token';
+
+
+export function getOrCreateTransaction(
+  id: String,
+  createIfNotFound: boolean = true,
+): Transaction {
+  // @ts-ignore: assign wrapper object to primitive
+  let transaction = Transaction.load(id);
+
+  if (transaction == null && createIfNotFound) {
+    // @ts-ignore: assign wrapper object to primitive
+    transaction = new Transaction(id);
+  }
+
+  return transaction as Transaction;
+}
+
+export function getOrCreateGeyserStakedEvent(id: String, gesyerId: String, event: Staked): StakedEvent {
+    let stake = StakedEvent.load(id.toString());
+    if (stake == null) {
+      stake = new StakedEvent(id.toString())
+    }
+
+    stake.amount = event.params.amount
+    stake.geyser = gesyerId.toString()
+    stake.user = event.params.user
+    stake.total = event.params.total
+    stake.timestamp = event.params.timestamp
+    stake.blockNumber = event.params.blockNumber
+    stake.data = event.params.data 
+    
+    return stake as StakedEvent
+
+}
+
+export function getOrCreateGeyserUnstakedEvent(id: String, gesyerId: String, event: Unstaked): UnstakedEvent {
+    let unstake = UnstakedEvent.load(id.toString());
+    if (unstake == null) {
+      unstake = new UnstakedEvent(id.toString())
+    }
+
+    unstake.amount = event.params.amount
+    unstake.geyser = gesyerId.toString()
+    unstake.user = event.params.user
+    unstake.total = event.params.total
+    unstake.timestamp = event.params.timestamp
+    unstake.blockNumber = event.params.blockNumber
+    unstake.data = event.params.data 
+    return unstake as UnstakedEvent
+}
+
+
+export function getOrCreateGeyser(geyserAddress: Address, update: boolean = true): Geyser {
+  let geyser = Geyser.load(geyserAddress.toHexString());
+  let geyserContract = BadgerGeyser.bind(geyserAddress);
+
+  if (geyser == null) {
+    geyser = new Geyser(geyserAddress.toHexString());
+    geyser.totalStaked = BIGINT_ZERO;
+  }
+
+  if (update) {
+    let totalStaked = geyserContract.try_totalStaked();
+    geyser.totalStaked = !totalStaked.reverted ? totalStaked.value : new BigInt(0);
+  }
+
+  return geyser as Geyser;
+}
