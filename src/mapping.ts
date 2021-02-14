0c4ff503430ca24e18dfd94e617a7f4aac994d9b
darruma <umerderr@gmail.com>
2021-02-14 21:20:14 +0000

    feat: added digg-wbtc harvest events to subgraph

--- src/mapping.ts
+++ src/mapping.ts
@@ -2,4 +2,4 @@ export { handleShareTransfer } from './mappings/yVault';
 export { handleHarvest } from './mappings/strategy';
 export { handleStaked, handleUnstaked } from './mappings/geyser';
 export { handleFarmHarvest } from './mappings/farm';
-export { handleSushiHarvest } from './mappings/sushi'
+export { handleSushiHarvest } from './mappings/sushi';
--- src/utils/helpers/sushi/harvest.ts
+++ src/utils/helpers/sushi/harvest.ts
@@ -1,21 +1,21 @@
-import { HarvestState } from '../../../../generated/harvestSushiWbtcEth/StrategySushiLpOptimizer'
-import { SushiHarvestEvent } from '../../../../generated/schema' 
+import { HarvestState } from '../../../../generated/harvestSushiWbtcEth/StrategySushiLpOptimizer';
+import { SushiHarvestEvent } from '../../../../generated/schema';
 
 export function getOrCreateSushiHarvestEvent(
-    id: String,
-    event: HarvestState,
+  id: String,
+  event: HarvestState,
 ): SushiHarvestEvent {
-    let sushiHarvestEvent = SushiHarvestEvent.load(id.toString())
-    if(sushiHarvestEvent == null) {
-        sushiHarvestEvent = new SushiHarvestEvent(id.toString())
-    }
-    sushiHarvestEvent.xSushiHarvested = event.params.xSushiHarvested
-    sushiHarvestEvent.totalxSushi = event.params.totalxSushi
-    sushiHarvestEvent.toStrategist = event.params.toStrategist
-    sushiHarvestEvent.toGovernance = event.params.toGovernance
-    sushiHarvestEvent.toBadgerTree = event.params.toBadgerTree
-    sushiHarvestEvent.timestamp = event.params.timestamp
-    sushiHarvestEvent.blockNumber = event.params.blockNumber
+  let sushiHarvestEvent = SushiHarvestEvent.load(id.toString());
+  if (sushiHarvestEvent == null) {
+    sushiHarvestEvent = new SushiHarvestEvent(id.toString());
+  }
+  sushiHarvestEvent.xSushiHarvested = event.params.xSushiHarvested;
+  sushiHarvestEvent.totalxSushi = event.params.totalxSushi;
+  sushiHarvestEvent.toStrategist = event.params.toStrategist;
+  sushiHarvestEvent.toGovernance = event.params.toGovernance;
+  sushiHarvestEvent.toBadgerTree = event.params.toBadgerTree;
+  sushiHarvestEvent.timestamp = event.params.timestamp;
+  sushiHarvestEvent.blockNumber = event.params.blockNumber;
 
-    return sushiHarvestEvent as SushiHarvestEvent
+  return sushiHarvestEvent as SushiHarvestEvent;
 }
