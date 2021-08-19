5a0d84c6ffe24c05d325170cd450be872bed093f
Darruma <umerderr@gmail.com>
2021-08-19 15:10:06 +0100

    feat: remove unused code nad change variable names

--- subgraphs/tokens-polygon/src/mappings/polygon.ts
+++ subgraphs/tokens-polygon/src/mappings/polygon.ts
@@ -3,15 +3,15 @@ import { Address } from '@graphprotocol/graph-ts';
 import { Transfer } from '../../generated/Badger/UChildERC20';
 import { getOrCreateTokenBalance } from '../utils/helpers/token/balance';
 export function handlePolygonTransfer(event: Transfer): void {
-  let badgerToken = event.address
+  let token = event.address
   let fromId = event.params.from
     .toHexString()
     .concat('-')
-    .concat(badgerToken.toHexString());
-  let toId = event.params.to.toHexString().concat('-').concat(badgerToken.toHexString());
+    .concat(token.toHexString());
+  let toId = event.params.to.toHexString().concat('-').concat(token.toHexString());
 
-  let fromAccount = getOrCreateTokenBalance(fromId, badgerToken);
-  let toAccount = getOrCreateTokenBalance(toId, badgerToken);
+  let fromAccount = getOrCreateTokenBalance(fromId, token);
+  let toAccount = getOrCreateTokenBalance(toId, token);
 
   fromAccount.balance = fromAccount.balance.minus(event.params.value);
   toAccount.balance = toAccount.balance.plus(event.params.value);
