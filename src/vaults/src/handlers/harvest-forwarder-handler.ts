681c734b4116884e788fe7787b31d98e46bede09
mitche50 <mitche50@gmail.com>
2022-03-17 23:22:10 -0400

    chore: re-use existing entity

--- src/vaults/src/handlers/harvest-forwarder-handler.ts
+++ src/vaults/src/handlers/harvest-forwarder-handler.ts
@@ -4,17 +4,16 @@ import {
   OwnerUpdate,
   Sweep,
 } from '../../generated/HarvestForwarder/HarvestForwarder';
-import { TreeDistribution as TreeDistributionEntity } from '../../generated/schema';
+import { BadgerTreeDistribution } from '../../generated/schema';
 
 export function handleTreeDistribution(event: TreeDistribution): void {
-  const entity = new TreeDistributionEntity(event.transaction.from.toHex());
+  let entity = new BadgerTreeDistribution(event.transaction.from.toHex());
 
   entity.id = event.transaction.hash.toHex() + '-' + event.logIndex.toString();
   entity.token = event.params.token.toHex();
   entity.amount = event.params.amount;
   entity.blockNumber = event.params.block_number;
-  entity.blockTimestamp = event.params.block_timestamp;
-  entity.beneficiary = event.params.beneficiary.toHex();
+  entity.sett = event.params.beneficiary.toHex();
 
   entity.save();
 }
