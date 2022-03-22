a068f0e2cc7632716b5defd1b48c05728b56a399
Swole Badger <swole@badger.finance>
2022-03-22 15:38:16 +0000

    feat: load strat entity

--- src/vaults/src/entities/badger-sett-v1-5.ts
+++ src/vaults/src/entities/badger-sett-v1-5.ts
@@ -3,6 +3,7 @@ import { Sett } from '../../generated/schema';
 import { BadgerSettV1_5 } from '../../generated/templates/BadgerSettV1_5/BadgerSettV1_5';
 import { ADDR_ZERO, NO_ADDR, ZERO } from '../constants';
 import { readValue } from './contracts';
+import { loadStrategy } from './strategy';
 import { loadToken } from './token';
 
 export function loadSettV1_5(address: Address): Sett {
@@ -31,8 +32,8 @@ export function loadSettV1_5(address: Address): Sett {
   }
 
   sett.available = readValue<BigInt>(contract.try_available(), sett.available);
-  sett.strategy = readValue<Address>(contract.try_strategy(), ADDR_ZERO).toHexString();
-
+  let strategy = readValue<Address>(contract.try_strategy(), ADDR_ZERO)
+  sett.strategy = loadStrategy(strategy).id
   sett.pricePerFullShare = readValue<BigInt>(
     contract.try_getPricePerFullShare(),
     sett.pricePerFullShare,
