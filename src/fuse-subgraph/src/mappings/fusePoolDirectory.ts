1a92d181d86740e228ce3d022bc3e3419ce8e6d4
mitche50 <mitche50@gmail.com>
2022-05-18 14:10:53 -0400

    chore: only use badger pool events

--- src/fuse-subgraph/src/mappings/fusePoolDirectory.ts
+++ src/fuse-subgraph/src/mappings/fusePoolDirectory.ts
@@ -1,4 +1,6 @@
 /* eslint-disable prefer-const */ // to satisfy AS compiler
+import { Address } from '@graphprotocol/graph-ts';
+import { BADGER_COMPTROLLER } from '../constants';
 import {
   PoolRegistered,
   // OwnershipTransferred
@@ -7,6 +9,7 @@ import { Comptroller } from '../types/templates';
 import { createPool } from './fusePools';
 
 export function handlePoolRegistered(event: PoolRegistered): void {
+  if (event.params.pool.comptroller != Address.fromString(BADGER_COMPTROLLER)) return;
   // Dynamically index all new listed Fuse pools
   Comptroller.create(event.params.pool.comptroller);
   // Create the pool for this comptroller, since it's now been registered.
