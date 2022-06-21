ca567c17f8e28979a8acb828ccb2e58b5d020c31
mitche50 <mitche50@gmail.com>
2022-06-21 13:47:48 -0400

    chore: vaults updates

--- src/vaults/src/handlers/bribe-processor-handler.ts
+++ src/vaults/src/handlers/bribe-processor-handler.ts
@@ -1,59 +1,23 @@
-import { BigDecimal, BigInt } from '@graphprotocol/graph-ts';
 import { TreeDistribution } from '../../generated/BribeProcessor/VotiumBribeProcessor';
-import {
-  ADDR_ZERO,
-  ADDR_BVECVX,
-  BADGER_SHARE,
-  OPS_FEE,
-  ADDR_BADGER,
-  ADDR_BVECVX_LP,
-} from '../constants';
+import { ADDR_ZERO, ADDR_BVECVX } from '../constants';
+import { loadSett } from '../entities/badger-sett';
 import { loadBadgerTreeDistribution } from '../entities/badger-tree-distribution';
-
-function determineBadgerSplit(totalAmount: BigInt): BigInt[] {
-  let totalAmountDecimal = BigDecimal.fromString(totalAmount.toString());
-  let percentBadgerbveCVX = 1 - (1 / BADGER_SHARE) * OPS_FEE;
-  let decimalPercentBadgerbveCVX = BigDecimal.fromString(percentBadgerbveCVX.toString());
-  let bveCVXAmount = BigInt.fromString(
-    totalAmountDecimal.times(decimalPercentBadgerbveCVX).truncate(0).toString(),
-  );
-  let bveCVXLpAmount = totalAmount.minus(bveCVXAmount);
-  return [bveCVXAmount, bveCVXLpAmount];
-}
+import { loadStrategy } from '../entities/strategy';
 
 export function handleTreeDistribution(event: TreeDistribution): void {
-  let amount = event.params.amount;
-  let splitAmount = BigInt.fromString('0');
-
-  if (event.params.token === ADDR_BADGER) {
-    let totalBadgerAmounts = determineBadgerSplit(amount);
-    amount = totalBadgerAmounts[0];
-    splitAmount = totalBadgerAmounts[1];
-
-    let lpDistribution = loadBadgerTreeDistribution(
-      event.params.timestamp,
-      event.params.blockNumber,
-      splitAmount,
-      event.params.token,
-      event.transaction.hash,
-      event.logIndex,
-    );
-    lpDistribution.sett = ADDR_BVECVX_LP.toHexString();
-    lpDistribution.strategy = ADDR_ZERO.toHexString();
-
-    lpDistribution.save();
-  }
-
   let distribution = loadBadgerTreeDistribution(
     event.params.timestamp,
     event.params.blockNumber,
-    amount,
+    event.params.amount,
     event.params.token,
     event.transaction.hash,
     event.logIndex,
   );
-  distribution.sett = ADDR_BVECVX.toHexString();
-  distribution.strategy = ADDR_ZERO.toHexString();
+
+  let sett = loadSett(ADDR_BVECVX);
+  distribution.sett = sett.id;
+  let strat = loadStrategy(ADDR_ZERO);
+  distribution.strategy = strat.id;
 
   distribution.save();
 }
