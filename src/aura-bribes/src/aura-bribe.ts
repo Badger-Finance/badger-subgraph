793689376783dd567cd67fd634781942d194f8dd
Andrew Kulikov <jadecoresky@gmail.com>
2022-06-29 21:26:48 +0300

    feat: add aura bribes subgraph

---
+++ src/aura-bribes/src/aura-bribe.ts
@@ -0,0 +1,14 @@
+import { DepositBribe, } from "../generated/BalancerBribe/BalancerBribe"
+import { Bribe } from "../generated/schema"
+
+export function handleDepositBribe(event: DepositBribe): void {
+  let bribe = new Bribe(event.transaction.hash.toHexString());
+
+  bribe.bribeIdentifier = event.params.bribeIdentifier;
+  bribe.proposal = event.params.proposal;
+  bribe.token = event.params.token;
+  bribe.amount = event.params.amount;
+  bribe.rewardIdentifier = event.params.rewardIdentifier;
+  bribe.briber = event.params.briber;
+  bribe.save();
+}
